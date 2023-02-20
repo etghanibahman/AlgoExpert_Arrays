2bcaf149bb1a8e6198f39de9213703c7e8530086
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-02-21 00:10:47 +0100

    initializing the test case

--- SpiralTraverse/Program.cs
+++ SpiralTraverse/Program.cs
@@ -7,7 +7,7 @@ namespace SpiralTraverse
     {
         static void Main(string[] args)
         {
-            int[,] array = null;
+            int[,] array = { { 1, 2, 3, 4}, {12, 13, 14, 5},{11, 16, 15, 6},{10, 9, 8, 7} };
             var output = SpiralTraverse(array);
             Console.WriteLine($"Output is: {String.Join<int>(',', output)} ");
         }
