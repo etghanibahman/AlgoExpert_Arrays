cd79f146d4ae4a1ed6197ebe855657c673acf472
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-02-20 23:53:57 +0100

    adding project - Spiral Traverse

---
+++ SpiralTraverse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpiralTraverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = null;
+            var output = SpiralTraverse(array);
+            Console.WriteLine($"Output is: {String.Join<int>(',', output)} ");
+        }
+
+        public static List<int> SpiralTraverse(int[,] array)
+        {
+            // Write your code here.
+            return new List<int>();
+        }
+    }
+}
