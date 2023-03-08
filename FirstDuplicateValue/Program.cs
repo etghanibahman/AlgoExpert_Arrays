47ec4364e4ab28eb74ce162239e58ea6c96de798
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-03-09 00:54:26 +0100

    First duplcate value - Simple solutions

--- FirstDuplicateValue/Program.cs
+++ FirstDuplicateValue/Program.cs
@@ -10,36 +10,55 @@ namespace FirstDuplicateValue
         static void Main(string[] args)
         {
             int[] array = new int[] { 2, 1, 5, 2, 3, 3, 4 };
+            //test case 14
+            array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             Console.WriteLine($"First Duplicate Value is : {FirstDuplicateValue(array)}");
         }
 
+
         public static int FirstDuplicateValue(int[] array)
         {
-            int result = -1;
-            //Dictionary<int, bool> dicNumbers = new Dictionary<int, bool>();
-            //var dicNumbers = array.ToLookup(a => new { num = a, value = false }); 
-            //Hashtable dicNumbers = new Hashtable();
-            List<(int key, bool value)> dicNumbers = new List<(int key, bool value)>();
+            var existingValues = new bool[array.Length + 1];
             for (int i = 0; i < array.Length; i++)
             {
-                dicNumbers.Add((array[i], false));
-            }
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (dicNumbers.FirstOrDefault(a => a.key == array[i]).value)
+                if (!existingValues[array[i]])
                 {
-                    result = array[i];
-                    break;
+                    existingValues[array[i]] = true;
                 }
                 else
                 {
-                    dicNumbers[i] = (array[i], true);
-                    //dicNumbers.FirstOrDefault(a => a.key == array[i]).value = true;
+                    return array[i];
                 }
             }
-            return result;
+            return -1;
         }
+
+        //public static int FirstDuplicateValue(int[] array)
+        //{
+        //    int result = -1;
+        //    //Dictionary<int, bool> dicNumbers = new Dictionary<int, bool>();
+        //    //var dicNumbers = array.ToLookup(a => new { num = a, value = false }); 
+        //    //Hashtable dicNumbers = new Hashtable();
+        //    List<(int key, bool value)> dicNumbers = new List<(int key, bool value)>();
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        dicNumbers.Add((array[i], false));
+        //    }
+
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        if (dicNumbers.FirstOrDefault(a => a.key == array[i]).value)
+        //        {
+        //            result = array[i];
+        //            break;
+        //        }
+        //        else
+        //        {
+        //            dicNumbers[i] = (array[i], true);
+        //        }
+        //    }
+        //    return result;
+        //}
 
         //public static int FirstDuplicateValue(int[] array)
         //{
